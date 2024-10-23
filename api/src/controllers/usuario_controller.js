94c1f7096e5c953cde418f021c7d26e624ad627d
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2024-10-23 16:25:26 -0300

    23/10

--- api/src/controllers/usuario_controller.js
+++ api/src/controllers/usuario_controller.js
@@ -39,6 +39,8 @@ module.exports = class usuario_controller {
             return res
               .status(201)
               .json({ message: "Cadastro realizado com sucesso." });
+
+              
           }
         });
       } catch (error) {
@@ -53,53 +55,83 @@ module.exports = class usuario_controller {
     if ( !email || !senha) {
       return res
         .status(400)
-        .json({ error: "Todos os campos devem ser preenchidos" });
+        .json({ error: " Todos os campos devem ser preenchidos" });
     } else if (!email.includes("@")) {
-      return res.status(400).json({ error: "Email inválido. Deve conter @" });
+      return res.status(400).json({ error: " Email inválido. Deve conter @" });
     }
-    let query = `SELECT * FROM usuario WHERE email=?;`
-    const email_check = [email];
+    let query = `SELECT * FROM usuario WHERE email=? AND senha=?;`
+    const login_check = [email, senha];
     
     try{
-        connect_database.query(query, email_check, function(err, results){ //"results" é um booleano, então ele retornará "true" ou "false"
+        connect_database.query(query, login_check, function(err, results){ 
            if(err){
             console.error(err);
             console.log(err.code);
             return res.status(500).json({
-                error: "Erro interno do servidor :("
+                error: " Erro interno do servidor :("
             });
            } 
            if(results.length === 0){
             return res.status(400).json({
-              error: "O email informado está incorreto ou não se encontra cadastrado no sistema"
+              error: " credenciais inválidas"
             });
            }
-           return res.status(200);
-        });
-        query = `SELECT * FROM usuario WHERE email=? AND senha=?;`
-        const senha_check = [email, senha];
-        connect_database.query(query, senha_check, function(err, results){
-          if(err){
-            console.error(err);
-            console.log(err.code);
-            return res.status(500).json({
-                error: "Erro interno do servidor :("
-            });
-          }
-          if(results.length === 0){
-            return res.status(400).json({
-              error: "A senha informada está incorreta"
-            });
-          }
+          //  const usuario = results[0];
+
+          //  if(usuario.senha != senha) {
+          //   return res.status(400).json({
+          //     error: " A senha informada está incorreta"
+          //   });
+
+          //  }
+          //  return res.status(200);
+
           return res.status(200).json({
             message: "Login efetuado."
           });
         });
+        // query = `SELECT * FROM usuario WHERE email=? AND senha=?;`
+        // const senha_check = [email, senha];
+        // connect_database.query(query, senha_check, function(err, results){
+        //   if(err){
+        //     console.error(err);
+        //     console.log(err.code);
+        //     return res.status(500).json({
+        //         error: " Erro interno do servidor :("
+        //     });
+        //   }
+        //   if(results.length === 0){
+        //     return res.status(400).json({
+        //       error: " A senha informada está incorreta"
+        //     });
+        //   }
+         
+        // });
     }
     catch(error){
       console.error(error);
-      res.status(500).json({error: "Erro interno do servidor"});
+      res.status(500).json({error: " Erro interno do servidor"});
     }
+  }
+  static async update_usuario(req, res) {
+    const {id, senha, nova_senha} = req.body;
+
+    if(!id, !senha, !nova_senha){
+      res.status(400).json({error: " todos os campos devem ser preenchidos"})
+    }
+
+    const query = `SELECT * FROM usuario WHERE id = ?`
+
+    connect_database.query(query, id, function(err, results){
+      if(err){
+        console.error(err);
+        console.log(err.code);
+        return res.status(500).json({
+            error: " Erro interno do servidor :("
+        });
+       } 
+       if(results.senha !== )
+    })
   }
 };
 
