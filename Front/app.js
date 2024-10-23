8a68d05f7848ab03b785ff14b538be7b2ebb23d9
livyamv <liviareis2702@gmail.com>
2024-10-23 14:14:59 -0300

    23d3

--- Front/app.js
+++ Front/app.js
@@ -51,3 +51,51 @@ function cadastrar_usuario(event) {
       console.error("Erro:", error.message);
     });
 }
+
+function login_usuario(event) {
+
+  //previne o comportamento padrão do formulário, ou seja, impede que ele saja enviado e recarregue a página
+  event.preventDefault();
+  //Captura os valores dos compos de formulario
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  //Requisção HTTP para o endpoint de cadastro de usuário
+  fetch("http://localhost:5000/Agenda-Senai/api/v1/usuario_cadastro/", {
+    //Realiza uma chamada HTTP para o servidor (a rota definida)
+    method: "POST",
+    headers: {
+      //A requisição será em formato json
+      "Content-Type": "application/json",
+    },
+    //Transforma os dados do formulário em uma string json para serem emviados no corpo da requisição
+    body: JSON.stringify({ senha, email}),
+  })
+    .then((response) => {
+      //Tratamento da resposta do servidor / API
+      if (response.ok) {
+        //Verifica se a resposta foi bem sucedida(status 2xx)
+        return response.json();
+      }
+      //Convertendo o erro em formato json
+      return response.json().then((err) => {
+        //Mensagem retonada do servidor,acessada pela chave "error"
+        throw new Error(err.error);
+      });
+    }) //fechamento da then(response)
+    .then((data) => {
+      //Executa a resposta de sucesso -retorna ao usuário final
+      //Exibe um alerta para o usuário final(front) com o nome do usuário que acabou de ser cadastrado
+      //alert("Usuário cadastrado com sucesso! " + data.user.name);
+      alert(data.message);
+      console.log(data.message);
+      
+      //Reseta os campos do formulário após osucesso do trabalho
+      document.getElementById("formulario-login").reset();
+    })
+    .catch((error) => {
+      //Captura qualquer erro que ocorra durente o processo da requisição /resposta
+      alert("Erro no login:" + error.message);
+      console.error("Erro:", error.message);
+    });
+}
